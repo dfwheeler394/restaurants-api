8a407cc73752282f8339e0eaee74dbeb414bed1e
Dean Wheeler <dfwheeler394@gmail.com>
2020-04-07 09:57:55 -0500

    create method to restaurants controller

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,6 +2,17 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   acts_as_token_authentication_handler_for User, except: [ :index, :show ]
   before_action :set_restaurant, only: [ :show, :update ]
 
+  def create
+   @restaurant = Restaurant.new(restaurant_params)
+   @restaurant.user = current_user
+   authorize @restaurant
+   if @restaurant.save
+     render :show, status: :created
+   else
+     render_error
+   end
+ end
+
   def update
     if @restaurant.update(restaurant_params)
       render :show
