3b7f8aa251511457a8b66409fa50090663aebe58
Dean Wheeler <dfwheeler394@gmail.com>
2020-04-07 09:57:04 -0500

    create route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :restaurants, only: [ :index, :show, :update  ]
+      resources :restaurants, only: [ :index, :update, :show, :update  ]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
