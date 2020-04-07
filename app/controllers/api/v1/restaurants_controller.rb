1bbf9dcff2fa3663ba77fa5a0f321eef29dcb243
Dean Wheeler <dfwheeler394@gmail.com>
2020-04-07 09:49:06 -0500

    restaurant view, controller updated

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  def index
-    @restaurants = policy_scope(Restaurant)
+  before_action :set_restaurant, only: [ :show ]
+
+  def show
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant  # For Pundit
   end
 end
